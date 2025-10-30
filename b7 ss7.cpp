b7df18a4a9bdccffd5288f00d644f0732063d30b
tiennamz <ghastnam001@gmail.com>
2025-10-30 22:44:57 +0700

    b7 ss7.cpp

--- b7 ss7.cpp
+++ b7 ss7.cpp
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-	int a,b;
-	printf("Nhap 2 khoang a va b : ");
-	scanf("%d%d",&a,&b);
-	if(a>=b){
-		for(int i=b;i<=a;i++){
-			printf("\n%d",i);
+	int n1,n2;
+
+	do{
+		printf("Nhap so nguyen duong n1: ");
+		scanf("%d", &n1);
+		if(n1<1){
+			printf("Nhap lai n1!\n");
+		}
+	}while(n1<1);
+
+	do{
+		printf("Nhap so nguyen duong n2: ");
+		scanf("%d", &n2);
+		if(n2<1 || n2<n1){
+			printf("Nhap lai n2!\n");
+		}
+	}while(n2<1 || n2<n1);
+	printf("\n\nCac so nguyen to tu %d den %d la: \n",n1,n2);
+	int demNT = 0;
+	for(int i=n1;i<=n2;i++){
+		int flag = 1;
+		for(int j=2;j<=sqrt(i);j++){
+			if(i%j==0){
+				flag = 0;
+				break;
+			}
+		}
+		if(flag==1 && i>1){
+			demNT++;
+			printf("%5d",i);
 		}
 	}
-	else if(b>=a){
-		for(int n=a;n<=b;n++){
-			printf("\n%d",n);
-		}
-	}
+	printf("\nCo %d so nguyen to",demNT);
 }
+
