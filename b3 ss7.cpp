5f14c5f36921e5a394e36a5316608ad19e7aa929
tiennamz <ghastnam001@gmail.com>
2025-10-30 22:37:09 +0700

    b3 ss7.cpp

--- b3 ss7.cpp
+++ b3 ss7.cpp
@@ -2,22 +2,24 @@
 #include <math.h>
 int main(){
 	int n;
-	int so_dao_nguoc=0;
+	int inversion=0;
 	int i;
-	int so_ban_dau;
-	so_ban_dau=n;
+	int first_number;
+	
 	printf("Nhap so nguyen n : ");
 	scanf("%d",&n);
+	first_number=n;
 	while(n!=0){
 		i=n%10;
-		so_dao_nguoc=so_dao_nguoc*10+i;
+		inversion=inversion*10+i;
 		n=n/10;
 	}
-	if(so_ban_dau==so_dao_nguoc){
+	if(first_number==inversion){
 		printf("Day la so doi xung");
 	}
 	else{
 		printf("Khong phai la so doi xung");
 	}
-	printf("%d",so_ban_dau);
+
 }
+
